aa1dc88475a054977f08776c8d47205fa9e4be42
Alex Meddock <alexmeddock@gmail.com>
2019-09-14 13:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function happyHolidays() {
   return "Happy Holidays!"
 }
+function happyHolidaysTo(name) {
+  return "Happy Holidays, ${name}!"
+}
+function happyHolidaysTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!"
+}
